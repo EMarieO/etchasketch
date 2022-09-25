c7ab8af090cb0880293816c50f7c16ed0b533d0c
Emma O <EmmaMKnutson1@msn.com>
2022-09-25 16:50:26 -0700

    added grid using loop and added css to create hover effect

---
+++ etch.js
@@ -0,0 +1,11 @@
+const container = document.querySelector('#container');
+
+const gridNumber = 255;
+for (let i = 0; i <= 255; i++) {
+   const content = document.createElement('div');
+   content.classList.add('content');
+  
+container.appendChild(content);
+
+}
+
