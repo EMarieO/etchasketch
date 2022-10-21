1d5cf8048e7be3d21411bd17cb4aaf2d96713cec
Emma O <EmmaMKnutson1@msn.com>
2022-10-20 18:54:48 -0700

    edited css and js to add functions for user prompt

--- etch.js
+++ etch.js
@@ -1,11 +1,35 @@
 const container = document.querySelector('#container');
 
-const gridNumber = 255;
-for (let i = 0; i <= 255; i++) {
+
+function createGrid(gridSize) {
+for (let i = 0; i < gridSize * gridSize; i++) {
    const content = document.createElement('div');
    content.classList.add('content');
-  
-container.appendChild(content);
+   container.style.gridTemplateColumns = `repeat(${gridSize}, 1fr)`
+   container.style.gridTemplateRows = `repeat(${gridSize}, 1fr)`
+   container.appendChild(content);
+   content.addEventListener('mouseover', () => {
+   content.style.backgroundColor = 'green';
+  });
+ }
+}
 
+createGrid(16);
+
+function removeOldGrid() {
+   while(container.firstChild) {
+      container.removeChild(container.firstChild);
+   }
+}
+
+function reset() {
+  let resetB = Number(window.prompt("New number of squares per side:", ""));
+  if (resetB > 100)
+    window.prompt("Needs to be a number under 100!");
+    else
+     removeOldGrid ();
+     createGrid(resetB);
+     
+    
 }
 
